c4f19df421f9544e3b6c5082431e81511dc9a645
budinurjaman <32738812+budinurjaman@users.noreply.github.com>
2017-10-23 21:05:13 +0700

    Add files via upload

---
+++ ProgramMemasakNasi.java
@@ -0,0 +1,20 @@
+//package program.memasak.nasi;
+
+public class ProgramMemasakNasi {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    System.out.println("CARA MEMASAK NASI");
+    System.out.println("1. Siapkan beras seberat 1.5 kg");
+    System.out.println("2. Cuci beras hingga bersih") ;
+    System.out.println("3. Tuangkan air pada beras yang sudah di cuci sebanyak 2 Liter") ;
+    System.out.println("4. Masukan beras yang sudah di isi air pada penanak nasi") ;
+    System.out.println("5. Nyalakan penanak nasi dan klik tombol ON pada penanak nasi") ;
+    System.out.println("6. Tunggu 25 menit sampai nasi matang") ;
+    System.out.println("7. Nasi siap untuk di santap") ;
+
+
+    }
+
+}
